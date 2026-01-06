70dad3f8ed9807d24377a404280f8d6255a285c8
richardios275 <kaylabridget1112@proton.me>
2026-01-06 13:56:03 +0700

    Made the register page prettier

--- register/index.php
+++ register/index.php
@@ -1,5 +1,5 @@
 <?php
-  session_start();
+session_start();
 ?>
 
 <!DOCTYPE html>
@@ -14,79 +14,87 @@
     integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
 </head>
 
-<body class="d-flex justify-content-center align-items-center py-4">
-  <main class="tuittuit-bg mb-2">
-    <form action="../actions/register_action.php" method="post" class="mx-4">
-      <div class="pb-5">
-        <h1 class="tuittuit-title">
-          tuit tuit</h1>
-      </div>
-      <h1>Create your account</h1>
-      <?php
-      if (isset($_SESSION["regist_errors"])) {
-        $errormessage = "";
-        for ($i = 0; $i < count($_SESSION["regist_errors"]); $i++ ) {
-          $errormessage .= $_SESSION["regist_errors"][$i] ."; ";
-        }
-        echo "<div class=\"alert alert-danger text-wrap\" role=\"alert\">". "An error occured: " . $errormessage ."</div>";
-        unset($_SESSION['regist_errors']);
-      }
-      ?>
+<body style="background-color: #8ace93ff;">
+  <section class="container-fluid d-flex justify-content-center align-items-center py-4">
 
-      <div class="form-floating mb-2">
-        <input type="text" class="form-control" id="usernameInput" name="username" placeholder="johnsmith7" required>
-        <label for="usernameInput" class="form-label">Username</label>
-      </div>
-      <div class="form-floating mb-2">
-        <input type="password" class="form-control" id="passwordInput" name="password" placeholder="Password" required>
-        <label for="passwordInput" class="form-label">Password</label>
-      </div>
-      <div class="mb-4">
-        <label>Date of birth (Optional)</label><br>
-        <small>We do not store this information at all, this is just for formalities</small>
-        <div class="row">
-          <div class="col">
-            <select name="day" id="daySelect" class="form-select" aria-label="Day">
-              <?php for ($i = 1; $i <= 31; $i++) { echo "<option value=\"$i\">$i</option>"; } ?>
-            </select>
-          </div>
-          <div class="col">
-            <select name="month" id="monthSelect" class="form-select" aria-label="Month">
-              <option value="1">January</option>
-              <option value="2">February</option>
-              <option value="3">March</option>
-              <option value="4">April</option>
-              <option value="5">May</option>
-              <option value="6">June</option>
-              <option value="7">July</option>
-              <option value="8">August</option>
-              <option value="9">September</option>
-              <option value="10">October</option>
-              <option value="11">November</option>
-              <option value="12">December</option>
-            </select>
-          </div>
-          <div class="col">
-            <select name="year" id="yearSelect" class="form-select" aria-label="Year">
-              <?php for ($i = 2026; $i > 1900; $i--) { echo "<option value=\"$i\">$i</option>"; } ?>
-            </select>
+
+    <main class="tuittuit-bg p-4 mb-2 rounded" style="box-shadow: 5px 5px 5px rgba(0, 0, 0, 0.3);">
+      <form action="../actions/register_action.php" method="post" class="mx-4">
+        <div class="pb-5">
+          <img src="/public/svg/logo.svg" alt="Tuit Tuit" height="48" background-color="">
+        </div>
+        <h1>Create your account</h1>
+        <?php
+        if (isset($_SESSION["regist_errors"])) {
+          $errormessage = "";
+          for ($i = 0; $i < count($_SESSION["regist_errors"]); $i++) {
+            $errormessage .= $_SESSION["regist_errors"][$i] . "; ";
+          }
+          echo "<div class=\"alert alert-danger text-wrap\" role=\"alert\">" . "An error occured: " . $errormessage . "</div>";
+          unset($_SESSION['regist_errors']);
+        }
+        ?>
+
+        <div class="form-floating mb-2">
+          <input type="text" class="form-control" id="usernameInput" name="username" placeholder="johnsmith7" required>
+          <label for="usernameInput" class="form-label">Username</label>
+        </div>
+        <div class="form-floating mb-2">
+          <input type="password" class="form-control" id="passwordInput" name="password" placeholder="Password"
+            required>
+          <label for="passwordInput" class="form-label">Password</label>
+        </div>
+        <div class="mb-4">
+          <label>Date of birth (Optional)</label><br>
+          <small>We do not store this information at all, this is just for formalities</small>
+          <div class="row">
+            <div class="col">
+              <select name="day" id="daySelect" class="form-select" aria-label="Day">
+                <?php for ($i = 1; $i <= 31; $i++) {
+                  echo "<option value=\"$i\">$i</option>";
+                } ?>
+              </select>
+            </div>
+            <div class="col">
+              <select name="month" id="monthSelect" class="form-select" aria-label="Month">
+                <option value="1">January</option>
+                <option value="2">February</option>
+                <option value="3">March</option>
+                <option value="4">April</option>
+                <option value="5">May</option>
+                <option value="6">June</option>
+                <option value="7">July</option>
+                <option value="8">August</option>
+                <option value="9">September</option>
+                <option value="10">October</option>
+                <option value="11">November</option>
+                <option value="12">December</option>
+              </select>
+            </div>
+            <div class="col">
+              <select name="year" id="yearSelect" class="form-select" aria-label="Year">
+                <?php for ($i = 2026; $i > 1900; $i--) {
+                  echo "<option value=\"$i\">$i</option>";
+                } ?>
+              </select>
+            </div>
           </div>
         </div>
-      </div>
-      <div class="mb-2 form-check">
-        <input type="checkbox" class="form-check-input" id="agreeCheckBox" required>
-        <label class="form-check-label" for="agreeCheckBox">By signing up, you agree to the <a target="_blank" href="#"
-            data-bs-toggle="modal" data-bs-target="#termsModal">Terms of Service</a> and <a target="_blank" href="#"
-            data-bs-toggle="modal" data-bs-target="#privacyModal">Privacy Policy</a></label>
-      </div>
-      <button id="submitButton" type="submit" class="btn btn-primary w-100 py-2 mb-4" disabled>Register</button>
-      <div class="d-flex justify-content-center">
-        <div>
-          Already have an account? <a href="../login">Login</a>
+        <div class="mb-2 form-check">
+          <input type="checkbox" class="form-check-input" id="agreeCheckBox" required>
+          <label class="form-check-label" for="agreeCheckBox">By signing up, you agree to the <a target="_blank"
+              href="#" data-bs-toggle="modal" data-bs-target="#termsModal">Terms of Service</a> and <a target="_blank"
+              href="#" data-bs-toggle="modal" data-bs-target="#privacyModal">Privacy Policy</a></label>
         </div>
-      </div>
-    </form>
-  </main>
+        <button id="submitButton" type="submit" class="btn btn-primary w-100 py-2 mb-4" disabled>Register</button>
+        <div class="d-flex justify-content-center">
+          <div>
+            Already have an account? <a href="../login">Login</a>
+          </div>
+        </div>
+      </form>
+    </main>
+  </section>
   <div id="termsModal" class="modal fade" tabindex="-1">
     <div class="modal-dialog modal-dialog-scrollable">
       <div class="modal-content">
